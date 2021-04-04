5a9207305059c41c05476e64f040c9a1a9e0e8e3
Alex Harley <alexbruceharley@gmail.com>
2021-04-04 19:59:04 +0200

    chore: purge _pages

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,7 +1,11 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './_pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: 'class',
   theme: {
     extend: {
