c310729b9c3aaec130e0a6833a3ea2060e6de7e9
Alex Harley <alexbruceharley@gmail.com>
2021-04-04 18:49:14 +0200

    app and landing refactor

--- packages/landing/tailwind.config.js
+++ packages/landing/tailwind.config.js
@@ -1,3 +1,26 @@
-const config = require('../../tailwind.config');
+const colors = require('tailwindcss/colors');
 
-module.exports = config;
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'gray-750': '#2a374a',
+        'gray-825': 'rgb(37, 47, 63)',
+        'gray-850': 'rgb(26, 32, 44)',
+
+        orange: colors.orange,
+
+        'celo-green': '#35D07F',
+        'celo-gold': '#FBCC5C',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      borderRadius: ['first', 'last'],
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
