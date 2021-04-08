aded7460093b818a4888312bd2e52e430d4f7571
Alex Harley <alexbruceharley@gmail.com>
2021-04-08 17:50:40 +0200

    chore: ring-0

--- packages/app/components/input.tsx
+++ packages/app/components/input.tsx
@@ -18,7 +18,7 @@ export function Input(props: InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
       {...clean}
-      className={`w-full appearance-none block px-3 py-2 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-600 w-64 ${className}`}
+      className={`w-full appearance-none block px-3 py-2 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-0 focus:border-indigo-500 sm:text-sm dark:bg-gray-600 w-64 ${className}`}
     />
   );
 }
