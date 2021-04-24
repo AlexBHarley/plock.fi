e2596d5d8dfef1411f3cff86e2b28812691c72ff
Alex Harley <alexbruceharley@gmail.com>
2021-04-24 10:57:46 +0200

    chore: fixing table overlfow

--- packages/app/components/table.tsx
+++ packages/app/components/table.tsx
@@ -19,10 +19,10 @@ export function Table({
   sort?: { property: string; desc: boolean };
 }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col overflow-x-hidden">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="overflow-hidden border-b border-gray-200 dark:border-gray-900">
+          <div className="border-b border-gray-200 dark:border-gray-900">
             <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-600">
               <thead className="">
                 <tr>
